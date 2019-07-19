ba4ac0b83ed1c5a073b1c8e6d9913560ff261dd1
muppet-training <tomcurphey12@gmail.com>
2019-07-19 14:26:57 +1000

    Installed styled components, styled a basic header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Page = styled.section`
+  max-width: 1320px;
+  margin: 0 auto;
+`;
+
+const Header = styled.section`
+  background-color: #def1f4;
+  padding: 10px;
+  height: 60px;
+  display: grid;
+  grid-template-columns: auto 200px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Page>
+      <Header>
+        <h1>Women's Tops</h1>
+        <form>
+          <input type="text" placeholder="filter by size" />
+        </form>
+      </Header>
+    </Page>
   );
 }
 
